4ea8ddb5c865e56ce0801a93b998843a80376ff0
whoant <vovanhoangtuan4.2@gmail.com>
2022-02-10 10:02:32 +0700

    Refactor code

--- content.js
+++ content.js
@@ -11,13 +11,13 @@ function injectHook(url, type = '') {
 
 // injectHook(chrome.extension.getURL('/lib/emoji.js'));
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.message === 'TabUpdated' && document.location.href.includes('https://www.facebook.com/stories')) {
-        // injectHook(chrome.extension.getURL(`/lib/story.js?v=${getRandom(1, 100)}`), 'module');
-        // injectHook(chrome.extension.getURL(`/lib/story.js`), 'module');
-        // chrome.tabs.executeScript(null, {file: 'lib/story.js'});
-    }
-})
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     if (request.message === 'TabUpdated' && document.location.href.includes('https://www.facebook.com/stories')) {
+//         // injectHook(chrome.extension.getURL(`/lib/story.js?v=${getRandom(1, 100)}`), 'module');
+//         // injectHook(chrome.extension.getURL(`/lib/story.js`), 'module');
+//         // chrome.tabs.executeScript(null, {file: 'lib/story.js'});
+//     }
+// })
 
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
